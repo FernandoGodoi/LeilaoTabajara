b5e4fc08dabe7a62d83ab7214ef3f0123c65030a
Fernando <tra_fernando@hotmail.com>
2017-11-16 09:55:25 -0200

    commit

---
+++ src/main/java/br/com/aaej/leilaotabajaraserver/Produto.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.aaej.leilaotabajaraserver;
+
+/**
+ *
+ * @author aluno
+ */
+public class Produto {
+    private String nome;
+    private String caracteristica;
+    private double precoInicial;
+    public Produto(String nome, String carac, double precoInicial){
+        this.nome = nome;
+        this.caracteristica = carac;
+        this.precoInicial = precoInicial;       
+    
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCaracteristica() {
+        return caracteristica;
+    }
+
+    public void setCaracteristica(String caracteristica) {
+        this.caracteristica = caracteristica;
+    }
+
+    public double getPrecoInicial() {
+        return precoInicial;
+    }
+
+    public void setPrecoInicial(double precoInicial) {
+        this.precoInicial = precoInicial;
+    }
+    @Override
+    public String toString(){
+        return this.nome+";"+this.precoInicial+";"+this.caracteristica;
+    }
+    
+}
